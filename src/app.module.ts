47b788c0ac85f68f4c3367aebd6017088ee1d331
Khursandov <xursandov_s@mail.ru>
2024-01-19 23:39:27 +0500

    feature: added student controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PaymentsModule } from '@module/payments/payments.module';
 import { GroupsModule } from '@module/groups/groups.module';
+import { StudentsModule } from '@module/students/students.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1/individDb'),
     GroupsModule,
     PaymentsModule,
+    StudentsModule,
   ],
   controllers: [],
   providers: [],
